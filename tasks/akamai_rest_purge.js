8113f68c8cc7a0bb4ca646b0a619a3e87db5f9cc
jeongsik-woo <jeongsik@dable.io>
2019-02-14 16:39:13 +0900

    v3 api 아카마이 라이브러리 추가
    기존의 HTTP api 직접 콜 대신 아카마이 라이브러리 이용하여 호출하도록
    변경 및 auth를 위한 인증 정보 추가
    
    exmaple
    - https://github.com/akamai/api-kickstart/tree/master/examples/node
    - https://www.npmjs.com/package/edgegrid

--- tasks/akamai_rest_purge.js
+++ tasks/akamai_rest_purge.js
@@ -8,7 +8,47 @@
 
 'use strict';
 
-var request = require('request');
+const EdgeGrid = require('edgegrid');
+const path = require('path');
+
+const edgercPath = path.join(__dirname, "../.edgerc");
+// The section of the .edgerc file to use for authentication
+const sectionName = "default";
+// Create a new instance of the EdgeGrid signing library
+const eg = new EdgeGrid({
+    path: edgercPath,
+    section: sectionName,
+    debug: false
+});
+
+function invalidate(objects_list, cb) {
+    const purgeObj = {
+      "hostname": "bc.akamaiapibootcamp.com",
+      "objects": objects_list
+    };
+    console.info("Adding data to queue: " + JSON.stringify(purgeObj));
+
+    eg.auth({
+        path: "/ccu/v3/invalidate/url",
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: purgeObj
+    });
+
+    return eg.send(function(data, response) {
+      data = JSON.parse(data);
+
+      if (response.statusCode !== 201) {
+        console.error('[AKAMAI_PURGE_HTTP_ERROR]', response.statusCode);
+        return cb(response.statusCode, {body: response.body, data: data});
+      }
+      console.info('responseStatusCode', response.statusCode);
+      console.info('responseBody', response.body);
+      console.info("data: ", data);
+
+      return cb(null, {body: response.body, data: data});
+    });
+}
 
 var formatResponse = function (response) {
   return response.detail +
@@ -20,42 +60,27 @@ var formatResponse = function (response) {
 module.exports = function(grunt) {
 
   grunt.registerMultiTask('akamai_rest_purge', 'Purging Akamai via their Rest Interface', function() {
-
     var done = this.async();
-
-    var options = this.options({
-      method: 'post',
-      json: {}
-    });
-
-    if (!options.action) {
-      options.action = 'invalidate';
+    let actionName = this.options.action;
+    if (!actionName) {
+      actionName = 'invalidate';
     }
+  
+    if (actionName === 'invalidate') {
+      console.info(`[BEGIN_INVALIDATE] - at:${new Date()}`);
+      return invalidate(this.data.objects, (err, data) => {
+        if (err) {
+          console.error('[AKAMAI_PURGE_ERROR]', err, data);
+          return grunt.log.errorlns(err);
+        }
 
-    if (options.action === 'invalidate') {
-      const network = options.network ? options.network : 'production';
-      options.url = `https://api.ccu.akamai.com/ccu/v3/invalidate/url/${network}`;
+        console.info('[DONE_AKAMAI_PURGE]', data);
+        grunt.log.ok(JSON.stringify(data));
+        done();
+      });
     } else {
       return done(new Error(`[INVALID_ACTION] - options:${JSON.stringify(optoins)}`));
     }
-
-    options.json.objects = this.data.objects;
-
-    request(options, function (err, response) {
-      if (err) {
-        console.error('[AKAMAI_PURGE_ERROR]', err);
-        return grunt.log.errorlns(err.message);
-      }
-      if (response.statusCode !== 201) {
-        console.error('[AKAMAI_PURGE_HTTP_ERROR]', response.statusCode);
-        return grunt.log.errorlns(response.body.detail || response.body);
-      }
-
-      console.info('[DONE_AKAMAI_PURGE]', response.body);
-      grunt.log.ok(formatResponse(response.body));
-      done();
-    });
-
   });
 
 };
